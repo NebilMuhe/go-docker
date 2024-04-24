3d46ed0f60a1501d59f639fde031774231309b89
Nebil Muhidin <nebilmuhidin57@gmail.com>
2024-04-24 14:49:00 +0300

    add the gin

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"response": "welcome to the home page"})
+	})
+
+	router.Run(":8000")
 }
